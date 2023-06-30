e9342460551c3c473ef4edaa7b95da5e9ea3c913
Gilles Heitaa <gheitaa@idt.pf>
2023-06-30 11:08:14 -1000

    overkill delete log

--- Scripts/backup-script.bat
+++ Scripts/backup-script.bat
@@ -1,1 +1,1 @@
-robocopy "C:\SupportBe\Scripts" "A:\OneDrive\Innovative Digital Technologies\Support BE - General\Git\Scripts" /LOG+:C:\SupportBe\Scripts\log.log
+robocopy "C:\SupportBe\Scripts" "A:\OneDrive\Innovative Digital Technologies\Support BE - General\Git\Scripts"
