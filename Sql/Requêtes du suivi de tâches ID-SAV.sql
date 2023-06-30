91f68162c87856e4b554b15441dd682683ab202c
Gilles Heitaa <gheitaa@idt.pf>
2023-06-30 11:03:02 -1000

    Ne pas afficher les tâches fermées

--- Sql/Requêtes du suivi de tâches ID-SAV.sql
+++ Sql/Requêtes du suivi de tâches ID-SAV.sql
@@ -4,5 +4,6 @@ SELECT u.Intitule, REPLACE(i.Description,'!','') as Description,  i.NomClient No
 FROM sav.Interventions i
 JOIN acl.Utilisateurs u on i.OperateurId = u.Id
 LEFT JOIN sav.InterventionDetails id on id.InterventionId = i.Id
-WHERE i.TypePlanification != 3
+WHERE i.Description like '!%'
+AND (i.TypePlanification != 3 OR i.FluxStatut != 3)
 order by u.Intitule desc, i.Code, i.PlanningDebut
