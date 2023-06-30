a2a973040431a15a408f4801ac8d5fe88bbdc3d7
Gilles Heitaa <gheitaa@idt.pf>
2023-06-30 11:01:08 -1000

    sql suivi des tâches id-sav

---
+++ Sql/Requêtes du suivi de tâches ID-SAV.sql
@@ -0,0 +1,8 @@
+SELECT u.Intitule, REPLACE(i.Description,'!','') as Description,  i.NomClient NomClient, i.Code Code, 
+    i.PlanningDebut PlanningDebut, i.PlanningFin PlanningFin, i.TypePlanification TypePlanification, 
+    i.FluxStatut FluxStatut, id.LibelleLong Objet, id.Description Detail, id.Notes Notes
+FROM sav.Interventions i
+JOIN acl.Utilisateurs u on i.OperateurId = u.Id
+LEFT JOIN sav.InterventionDetails id on id.InterventionId = i.Id
+WHERE i.TypePlanification != 3
+order by u.Intitule desc, i.Code, i.PlanningDebut
