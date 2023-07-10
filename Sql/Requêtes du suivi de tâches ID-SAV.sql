edb9c4d09747e6a6c4755bdfbaac3b2c358563c9
Gilles Heitaa <gheitaa@idt.pf>
2023-07-10 10:02:33 -1000

    Fix sql suivi tache idsav

--- Sql/Requêtes du suivi de tâches ID-SAV.sql
+++ Sql/Requêtes du suivi de tâches ID-SAV.sql
@@ -1,9 +1,13 @@
-SELECT u.Intitule Intervenant, i.NomClient NomClient, REPLACE(i.Description,'!','') as Description, id.Notes Notes,
-	i.Code Code, i.PlanningDebut PlanningDebut, i.PlanningFin PlanningFin, i.TypePlanification TypePlanification, 
-    i.FluxStatut FluxStatut, id.LibelleLong Objet, id.Description Detail
-FROM sav.Interventions i
-JOIN acl.Utilisateurs u on i.OperateurId = u.Id
-LEFT JOIN sav.InterventionDetails id on id.InterventionId = i.Id
-WHERE i.Description like '!%'
-AND (i.TypePlanification != 3 OR i.FluxStatut != 3)
-order by u.Intitule desc, i.Code, i.PlanningDebut
+SELECT * FROM 
+(
+	SELECT u.Intitule Intervenant, i.NomClient NomClient, REPLACE(i.Description,'!','') as Description, id.Notes Notes,
+		i.Code Code, i.PlanningDebut PlanningDebut, i.PlanningFin PlanningFin, i.TypePlanification TypePlanification, 
+	    i.FluxStatut FluxStatut, id.LibelleLong Objet, id.Description Detail
+	FROM sav.Interventions i
+	JOIN acl.Utilisateurs u on i.OperateurId = u.Id
+	LEFT JOIN sav.InterventionDetails id on id.InterventionId = i.Id
+	WHERE i.Description like '!%'
+	AND i.TypePlanification != 3
+) t
+WHERE t.FluxStatut != 3
+order by t.Intervenant desc, t.Code, t.PlanningDebut
