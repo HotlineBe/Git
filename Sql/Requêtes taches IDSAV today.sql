1bfac5eb4d482f81b581db6bc12cece80e3ffe27
Gilles Heitaa <gheitaa@idt.pf>
2023-07-12 10:58:44 -1000

    Fix des données à afficher (mais pas convaincu)

--- Sql/Requêtes taches IDSAV today.sql
+++ Sql/Requêtes taches IDSAV today.sql
@@ -1,4 +1,4 @@
-select * from (
+select distinct * from (
 	SELECT u.Intitule Intervenant, i.NomClient NomClient, REPLACE(i.Description,'!','') as Description, id.Notes Notes,
 			i.Code Code, i.PlanningDebut PlanningDebut, i.PlanningFin PlanningFin, i.TypePlanification TypePlanification, 
 		    i.FluxStatut FluxStatut, id.LibelleLong Objet, id.Description Detail
@@ -15,6 +15,15 @@ select * from (
 	JOIN acl.Utilisateurs u on i.OperateurId = u.Id
 	LEFT JOIN sav.InterventionDetails id on id.InterventionId = i.Id
 	WHERE u.Intitule in ('Gilles', 'Stéphanie', 'Leilanie')
-	AND convert(varchar, i.PlanningFin, 23) = convert(varchar, (getdate()+1), 23)
+	AND convert(varchar, i.PlanningFin, 23) = convert(varchar, getdate(), 23)
+	UNION ALL 
+	SELECT u.Intitule Intervenant, i.NomClient NomClient, REPLACE(i.Description,'!','') as Description, id.Notes Notes,
+			i.Code Code, i.PlanningDebut PlanningDebut, i.PlanningFin PlanningFin, i.TypePlanification TypePlanification, 
+		    i.FluxStatut FluxStatut, id.LibelleLong Objet, id.Description Detail
+	FROM sav.Interventions i
+	JOIN acl.Utilisateurs u on i.OperateurId = u.Id
+	LEFT JOIN sav.InterventionDetails id on id.InterventionId = i.Id
+	WHERE u.Intitule in ('Gilles', 'Stéphanie', 'Leilanie')
+	AND convert(varchar, i.PlanningFin, 23) = convert(varchar, (getdate() + 1), 23)
 ) t 
 ORDER BY t.Intervenant
