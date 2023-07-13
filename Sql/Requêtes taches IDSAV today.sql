4a0f34a318224ea6a1cd78c6c1b387b5d3bc2bb7
Gilles Heitaa <gheitaa@idt.pf>
2023-07-13 09:10:25 -1000

    Modifier code flux et planification le libellé

--- Sql/Requêtes taches IDSAV today.sql
+++ Sql/Requêtes taches IDSAV today.sql
@@ -1,7 +1,18 @@
 select distinct * from (
 	SELECT u.Intitule Intervenant, i.NomClient NomClient, REPLACE(i.Description,'!','') as Description, id.Notes Notes,
-			i.Code Code, i.PlanningDebut PlanningDebut, i.PlanningFin PlanningFin, i.TypePlanification TypePlanification, 
-		    i.FluxStatut FluxStatut, id.LibelleLong Objet, id.Description Detail
+			i.Code Code, i.PlanningDebut PlanningDebut, i.PlanningFin PlanningFin,
+		    CASE i.TypePlanification
+		    	WHEN 0 THEN 'Non planifié'
+		        WHEN 1 THEN 'A planifier'
+		        WHEN 2 THEN 'En cours'
+		        WHEN 3 THEN 'Terminé'
+		    END AS TypePlanification,
+		    CASE i.FluxStatut
+		    	WHEN 0 THEN 'Nouveau'
+		        WHEN 1 THEN 'En cours'
+		        WHEN 3 THEN 'Fermé'
+		    END AS FluxStatut,
+		    id.LibelleLong Objet, id.Description Detail
 	FROM sav.Interventions i
 	JOIN acl.Utilisateurs u on i.OperateurId = u.Id
 	LEFT JOIN sav.InterventionDetails id on id.InterventionId = i.Id
@@ -9,8 +20,19 @@ select distinct * from (
 	AND convert(varchar, i.PlanningDebut, 23) = convert(varchar, getdate(), 23)
 	UNION ALL 
 	SELECT u.Intitule Intervenant, i.NomClient NomClient, REPLACE(i.Description,'!','') as Description, id.Notes Notes,
-			i.Code Code, i.PlanningDebut PlanningDebut, i.PlanningFin PlanningFin, i.TypePlanification TypePlanification, 
-		    i.FluxStatut FluxStatut, id.LibelleLong Objet, id.Description Detail
+			i.Code Code, i.PlanningDebut PlanningDebut, i.PlanningFin PlanningFin,
+		    CASE i.TypePlanification
+		    	WHEN 0 THEN 'Non planifié'
+		        WHEN 1 THEN 'A planifier'
+		        WHEN 2 THEN 'En cours'
+		        WHEN 3 THEN 'Terminé'
+		    END AS TypePlanification,
+		    CASE i.FluxStatut
+		    	WHEN 0 THEN 'Nouveau'
+		        WHEN 1 THEN 'En cours'
+		        WHEN 3 THEN 'Fermé'
+		    END AS FluxStatut,
+		    id.LibelleLong Objet, id.Description Detail
 	FROM sav.Interventions i
 	JOIN acl.Utilisateurs u on i.OperateurId = u.Id
 	LEFT JOIN sav.InterventionDetails id on id.InterventionId = i.Id
@@ -18,8 +40,19 @@ select distinct * from (
 	AND convert(varchar, i.PlanningFin, 23) = convert(varchar, getdate(), 23)
 	UNION ALL 
 	SELECT u.Intitule Intervenant, i.NomClient NomClient, REPLACE(i.Description,'!','') as Description, id.Notes Notes,
-			i.Code Code, i.PlanningDebut PlanningDebut, i.PlanningFin PlanningFin, i.TypePlanification TypePlanification, 
-		    i.FluxStatut FluxStatut, id.LibelleLong Objet, id.Description Detail
+			i.Code Code, i.PlanningDebut PlanningDebut, i.PlanningFin PlanningFin,
+		    CASE i.TypePlanification
+		    	WHEN 0 THEN 'Non planifié'
+		        WHEN 1 THEN 'A planifier'
+		        WHEN 2 THEN 'En cours'
+		        WHEN 3 THEN 'Terminé'
+		    END AS TypePlanification,
+		    CASE i.FluxStatut
+		    	WHEN 0 THEN 'Nouveau'
+		        WHEN 1 THEN 'En cours'
+		        WHEN 3 THEN 'Fermé'
+		    END AS FluxStatut,
+		    id.LibelleLong Objet, id.Description Detail
 	FROM sav.Interventions i
 	JOIN acl.Utilisateurs u on i.OperateurId = u.Id
 	LEFT JOIN sav.InterventionDetails id on id.InterventionId = i.Id
