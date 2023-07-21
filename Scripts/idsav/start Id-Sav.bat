06a09a306c1984c5c5bf7e7b9e921f96519d6219
Gilles Heitaa <gheitaa@idt.pf>
2023-07-21 11:19:46 -1000

    Delete doublon + up temps d'attente

--- Scripts/idsav/start Id-Sav.bat
+++ Scripts/idsav/start Id-Sav.bat
@@ -3,6 +3,6 @@ taskkill /IM Lexi.Desktop.exe
 taskkill /IM Lexi.Desktop.exe
 timeout /t 2
 start A:\App\IDSAV\Lexi.Desktop.exe
-timeout /t 5
+timeout /t 10
 call sendkeys.bat "lexi" ""
 exit
--- Scripts/start Id-Sav.bat
+++
@@ -1,9 +0,0 @@
-ipconfig /flushdns
-taskkill /IM Lexi.Desktop.exe
-timeout /t 2
-taskkill /IM Lexi.Desktop.exe
-timeout /t 2
-start C:\Lexi\Console\Lexi.Desktop.exe
-timeout /t 5
-call sendkeys.bat "lexi" ""
-exit
